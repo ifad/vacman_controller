647b2a9a1ee0953e8a4ba8d9b412b1afa86c88f1
Marcello Barnaba <vjt@openssl.it>
2019-01-03 13:19:48 +0100

    Add -rpath to the resulting object file

--- ext/vacman_controller/extconf.rb
+++ ext/vacman_controller/extconf.rb
@@ -3,6 +3,7 @@ require 'mkmf'
 VACMAN_CONTROLLER = ENV['VACMAN_PATH'] || '/opt/vasco/VACMAN_Controller-3.15.1'
 
 append_cflags "-I#{VACMAN_CONTROLLER}/include"
+append_ldflags "-L#{VACMAN_CONTROLLER}/lib -laal2sdk -Wl,-rpath #{VACMAN_CONTROLLER}/lib"
 
 if find_library('aal2sdk', 'AAL2DPXInit', "#{VACMAN_CONTROLLER}/lib")
   create_makefile('vacman_controller/vacman_controller')
