038699ac6df5c478ea85fc858028cf02537a898f
Marcello Barnaba <vjt@openssl.it>
2019-01-03 13:27:08 +0100

    Remove hardcoded library version

--- ext/vacman_controller/extconf.rb
+++ ext/vacman_controller/extconf.rb
@@ -1,6 +1,17 @@
 require 'mkmf'
 
-VACMAN_CONTROLLER = ENV['VACMAN_PATH'] || '/opt/vasco/VACMAN_Controller-3.15.1'
+def find_vacman_controller
+  Dir.glob('/opt/vasco/VACMAN_Controller-*').sort.reverse.first
+end
+
+VACMAN_CONTROLLER = ENV['VACMAN_PATH'] || find_vacman_controller
+
+if VACMAN_CONTROLLER
+  puts "Using VACMAN controller from #{VACMAN_CONTROLLER}"
+else
+  puts "No VASCO Vacman controller found in /opt/vasco"
+  exit 1
+end
 
 append_cflags "-I#{VACMAN_CONTROLLER}/include"
 append_ldflags "-L#{VACMAN_CONTROLLER}/lib -laal2sdk -Wl,-rpath #{VACMAN_CONTROLLER}/lib"
