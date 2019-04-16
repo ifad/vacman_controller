55bfee22b0078224c5d49f26478ea58499da9a07
Marcello Barnaba <vjt@openssl.it>
2019-04-16 12:55:01 +0200

    Use a single CI script

---
+++ ci/standalone.sh
@@ -0,0 +1,9 @@
+# This script runs all that travis runs.
+#
+set -ex
+
+export CC_TEST_REPORTER_ID=47866d6bec01d504297b03fb6b387103da34b1ac72a7bad784eaf52d85fc7bf8
+
+docker build -t vacman_controller -f ci/Dockerfile .
+sh -x ci/prepare_env.sh
+docker run --interactive --tty --env-file ci/env --workdir /build vacman_controller sh ci/run_build.sh
